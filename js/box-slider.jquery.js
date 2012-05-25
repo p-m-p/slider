ae7e57d72cb9b67a1bc500bb9b99f1eddee2aefe
Phil Parsons <phil@profilepicture.co.uk>
2012-05-25 09:08:03 +0100

    added start of just top and front slide switching with next and prev

--- js/box-slider.jquery.js
+++ js/box-slider.jquery.js
@@ -6,11 +6,54 @@
     
     return this.each(function () {
       
-      $(this).addClass("show-top");
-      
+      var $this = $(this)
+        , $next = $('.bs-next')
+        , $prev = $('.bs-prev')
+        , $slides = $this.find('.slide');
+
+      $next.on('click', nextSlide);
+      $prev.on('click', prevSlide);
+      $next.add($prev).data('bsbox', $this);
+      $this.data('bsangle', 0);
+
+      $slides.each(function (i, el) {
+
+        var $slide = $(el);
+
+        if (i > 1) {
+          $slide.hide();
+        }
+        if (i === 0) {
+          $slide.addClass('front');
+          $this.data('bscurrent', $slide);
+        }
+        if (i === 1) {
+          $slide.addClass('top');
+        }
+
+      });
+
     });
     
   };
+
+
+  // private
+  var nextSlide = function (ev) {
+    
+    var $box = $(this).data('bsbox')
+      , angle = $box.data('bsangle') + 90; 
+    $box.data('bsangle', angle);
+    $box.css('-webkit-transform', 'translateZ(-200px) rotateX(-' + angle + 'deg)');
+    ev.preventDefault();
+
+  };
+
+
+  var prevSlide = function (ev) {
+
+  }
+
   
   $.fn.boxSlider = function (m) {
     
