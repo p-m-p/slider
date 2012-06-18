e4bef6c9616fad57a0bbdf715449347f916ef87f
Phil Parsons <phil@profilepicture.co.uk>
2012-06-18 22:57:21 +0100

    updated example adaptor

--- js/animation-adaptor-example.js
+++ js/animation-adaptor-example.js
@@ -4,10 +4,15 @@
 
     var adaptor = {};
 
+    // setup slide and box css
     adaptor.initialize = function ($box, $slides, settings) {
-      $slides.filter(':gt(0)').hide();
+      $box.css('position', 'relative');
+      $slides
+        .css({ position: 'absolute', top: 0, left: 0 })
+        .filter(':gt(0)').hide();
     };
 
+    // fade current out and next in
     adaptor.transition = function (settings) {
       settings.$nextSlide.fadeIn(settings.speed);
       settings.$currSlide.fadeOut(settings.speed);
