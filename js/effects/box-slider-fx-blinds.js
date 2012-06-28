6ec21706cc28a16e6ead0c4f4dbad8da7ba394cc
Phil Parsons <phil@profilepicture.co.uk>
2012-06-28 22:11:08 +0100

    adding start of blinds fx

---
+++ js/effects/box-slider-fx-blinds.js
@@ -0,0 +1,76 @@
+;(function (w, $, undefined) {
+
+  w.jqBoxSlider.registerAnimator('blind', (function () {
+
+    var adaptor = {};
+
+    adaptor.initialize = function ($box, $slides, settings) {
+      var wrapper = document.createElement('div')
+        , imgSrc = $slides.eq(0).find('img').attr('src')
+        , blinds = []
+        , blindSize = $box.width() / 10
+        , fromLeft
+        , i = 0;
+
+      for (; i < 10; ++i) {
+        fromLeft = (i * blindSize);
+        blinds[i] = document.createElement('div');
+        blinds[i].style.position = 'absolute';
+        blinds[i].style.top = '0px';
+        blinds[i].style.left = fromLeft + 'px';
+        blinds[i].style.width = blindSize + 'px';
+        blinds[i].style.height = '100%';
+        blinds[i].style.backgroundImage = 'url(' + imgSrc + ')';
+        blinds[i].style.backgroundPosition = -fromLeft + 'px 0px';
+        wrapper.appendChild(blinds[i]);
+      }
+      
+      $box.css('position', 'relative');
+      $box.css('height', $slides.css('height'));
+      $slides.css({zIndex: 1, position: 'absolute', top: 0, left: 0});
+      wrapper.style.position = 'absolute';
+      wrapper.style.top = '0px';
+      wrapper.style.left = '0px';
+      wrapper.style.width = '100%';
+      wrapper.style.height = '100%';
+      wrapper.style.zIndex = 2;
+      $box.append(wrapper);
+
+      settings.blinds = wrapper;
+
+    };
+
+    adaptor.transition = function (settings) {
+      var intv = settings.speed / 10
+        , height = settings.$box.height()
+        , $blinds = $(settings.blinds).children();
+
+      console.log(settings.$slides.filter(function (i) { 
+        return settings.$slides.get(i) !== settings.blinds; 
+      }).hide());
+      settings.$nextSlide.show();
+
+      $blinds.each(function (i, el) {
+        var delay = intv * i
+          , $el = $(el); 
+
+        (function () {
+          setTimeout(function () {
+            $el.animate({top: height}, settings.speed); 
+          }, delay);
+        }());
+      });
+      
+      setTimeout(function () {
+        $blinds.css({
+            backgroundImage: 'url(' + settings.$nextSlide.find('img').attr('src') + ')'
+          , top: 0
+        });
+      }, settings.speed * 2);
+    };
+
+    return adaptor;
+
+  }()));
+
+}(window, jQuery || Zepto));
