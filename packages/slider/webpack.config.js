2fd2b2e2fac452a34a802b4212a1e3944daf3283
Phil Parsons <p-m-p@users.noreply.github.com>
2021-12-05 21:37:08 +0000

    build: tidy packages

--- packages/slider/webpack.config.js
+++ packages/slider/webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "umd"
   },
 };
