8a1e28c67bd3922f839dba5e9a426052db5d6d59
Phil Parsons <p-m-p@users.noreply.github.com>
2021-12-06 11:08:33 +0000

    build: update version of angular peer dependencies

--- packages/angular/update-version.js
+++ packages/angular/update-version.js
@@ -4,6 +4,7 @@ const version = require('./package.json').version;
 const packageJson = require(packageJsonPath);
 
 packageJson.version = version;
+packageJson.peerDependencies['@boxslider/slider'] = `^${version}`;
 fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8', () => {
-  console.log(`Updated ${packageJsonPath} with version ${version}`);
+  console.log(`Updated ${packageJsonPath} for version ${version}`);
 });
