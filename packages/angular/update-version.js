8cb8c30e71eec98b0d5fcc16986b7ef1f6cef878
Phil Parsons <p-m-p@users.noreply.github.com>
2021-12-07 08:24:48 +0000

    build: track peer dependency from root package

--- packages/angular/update-version.js
+++ packages/angular/update-version.js
@@ -1,10 +1,14 @@
 const packageJsonPath = './dist/boxslider/angular/package.json'
 const fs = require('fs');
-const version = require('./package.json').version;
+const rootPackageJson = require('./package.json');
 const packageJson = require(packageJsonPath);
 
+const corePackage = '@boxslider/slider';
+const version = rootPackageJson.version;
+const coreLibVersion = rootPackageJson.dependencies[corePackage]
+
 packageJson.version = version;
-packageJson.peerDependencies['@boxslider/slider'] = `^${version}`;
+packageJson.peerDependencies[corePackage] = `^${coreLibVersion}`;
 fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8', () => {
   console.log(`Updated ${packageJsonPath} for version ${version}`);
 });
