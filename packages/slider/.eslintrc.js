ab898ff6f67743970c45bf365089a4524996f1b0
Phil Parsons <p-m-p@users.noreply.github.com>
2022-07-23 16:13:47 +0100

    build(deps): update outdated dependencies

--- packages/slider/.eslintrc.js
+++ packages/slider/.eslintrc.js
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  rules: {
-  }
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+  rules: {}
 };
