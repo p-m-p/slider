eab78c3e19de0ffb0b567fd9812126ae334c8a20
Phil Parsons <p-m-p@users.noreply.github.com>
2022-10-03 10:52:44 +0100

    fix: add build script with bundling

---
+++ build.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import { build } from 'esbuild'
+import { join } from 'path'
+
+const srcDir = join(process.cwd(), 'src')
+const distDir = join(process.cwd(), 'dist')
+
+const options = {
+  entryPoints: [join(srcDir, 'index.ts')],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  target: 'esnext',
+  tsconfig: '../../tsconfig.json',
+}
+
+build({
+  ...options,
+  outdir: join(distDir, 'esm'),
+  format: 'esm',
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
+
+build({
+  ...options,
+  outdir: join(distDir, 'cjs'),
+  format: 'cjs',
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
