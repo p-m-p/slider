78b7d6b4cd6ea7d13d75a97962a033dff37f4aaf
Phil Parsons <p-m-p@users.noreply.github.com>
2023-02-27 08:31:33 +0000

    feat: add icon

--- packages/examples/src/layout/Header.tsx
+++ packages/examples/src/layout/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="container mx-auto flex justify-between items-center p-4">
         <h1 className="text-xl font-mono">
           <Link to="/slider">
-            <span className="text-neutral-400">&lt;</span>BoxSlider<span className="text-neutral-400">&gt;</span>
+            <img src="/logo-small.webp" alt="Box slider" width="150" height="33" />
           </Link>
         </h1>
       </div>
