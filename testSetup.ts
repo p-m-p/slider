6c65f38e501c009f8f2e4ae0bef2197910eb3850
Phil Parsons <p-m-p@users.noreply.github.com>
2024-08-29 19:51:22 +0100

    test: stub missing animations api

--- testSetup.ts
+++ testSetup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/vitest'
+
+if (typeof HTMLElement.prototype.animate === 'undefined') {
+  HTMLElement.prototype.animate = vi.fn(() => ({
+    finished: Promise.resolve(),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  })) as any
+  HTMLElement.prototype.getAnimations = vi.fn(() => [])
+}
