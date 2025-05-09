005c919908471ae1837cf7ec56c98316fe1a0f3c
Phil Parsons <p-m-p@users.noreply.github.com>
2025-05-09 08:09:49 +0100

    feat(slider): start adding shorthand slider creation helpers

--- packages/slider/src/index.ts
+++ packages/slider/src/index.ts
@@ -1,3 +1,39 @@
-export { default as BoxSlider, defaultOptions } from './box-slider'
+import type { BoxSliderOptions } from './types'
+import {
+  default as FadeSlider,
+  type FadeSliderOptions,
+} from './effects/fade-slider'
+import { default as BoxSlider, defaultOptions } from './box-slider'
+
+export type CreateFadeSliderOptions = Partial<BoxSliderOptions> &
+  FadeSliderOptions
+
+export function createFadeSlider(
+  el: string | HTMLElement,
+  options: CreateFadeSliderOptions,
+) {
+  const { timingFunction, ...boxSliderOptions } = options
+  let sliderEl: HTMLElement
+
+  if (typeof el === 'string') {
+    const queryEl = document.querySelector<HTMLElement>(el)
+
+    if (queryEl === null) {
+      throw new Error(`Slider element not found: ${el}`)
+    }
+
+    sliderEl = queryEl
+  } else {
+    sliderEl = el
+  }
+
+  return new BoxSlider(
+    sliderEl,
+    new FadeSlider({ timingFunction }),
+    boxSliderOptions,
+  )
+}
+
+export { BoxSlider, defaultOptions }
 export * from './types'
 export * from './effects'
