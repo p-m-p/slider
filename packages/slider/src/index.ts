b3e7badf5d1cc2b401c8a0c33c3564d13526e859
Phil Parsons <p-m-p@users.noreply.github.com>
2025-05-09 08:24:05 +0100

    feat(slider): add shorthand creator functions for all effects

--- packages/slider/src/index.ts
+++ packages/slider/src/index.ts
@@ -3,7 +3,65 @@ import {
   default as FadeSlider,
   type FadeSliderOptions,
 } from './effects/fade-slider'
+import {
+  default as CarouselSlider,
+  type CarouselSliderOptions,
+} from './effects/carousel-slider'
+import {
+  default as CubeSlider,
+  type CubeSliderOptions,
+} from './effects/cube-slider'
+import {
+  default as TileSlider,
+  type TileSliderOptions,
+} from './effects/tile/tile-slider'
 import { default as BoxSlider, defaultOptions } from './box-slider'
+
+export function getSliderElement(el: string | HTMLElement): HTMLElement {
+  if (typeof el === 'string') {
+    const queryEl = document.querySelector<HTMLElement>(el)
+
+    if (queryEl === null) {
+      throw new Error(`Slider element not found: ${el}`)
+    }
+
+    return queryEl
+  }
+
+  return el
+}
+
+export type CreateCarouselSliderOptions = Partial<BoxSliderOptions> &
+  CarouselSliderOptions
+
+export function createCarouselSlider(
+  el: string | HTMLElement,
+  options: CreateCarouselSliderOptions,
+) {
+  const { timingFunction, cover, ...boxSliderOptions } = options
+
+  return new BoxSlider(
+    getSliderElement(el),
+    new CarouselSlider({ timingFunction, cover }),
+    boxSliderOptions,
+  )
+}
+
+export type CreateCubeSliderOptions = Partial<BoxSliderOptions> &
+  CubeSliderOptions
+
+export function createCubeSlider(
+  el: string | HTMLElement,
+  options: CreateCubeSliderOptions,
+) {
+  const { direction, perspective, ...boxSliderOptions } = options
+
+  return new BoxSlider(
+    getSliderElement(el),
+    new CubeSlider({ direction, perspective }),
+    boxSliderOptions,
+  )
+}
 
 export type CreateFadeSliderOptions = Partial<BoxSliderOptions> &
   FadeSliderOptions
@@ -13,23 +71,26 @@ export function createFadeSlider(
   options: CreateFadeSliderOptions,
 ) {
   const { timingFunction, ...boxSliderOptions } = options
-  let sliderEl: HTMLElement
-
-  if (typeof el === 'string') {
-    const queryEl = document.querySelector<HTMLElement>(el)
-
-    if (queryEl === null) {
-      throw new Error(`Slider element not found: ${el}`)
-    }
-
-    sliderEl = queryEl
-  } else {
-    sliderEl = el
-  }
 
   return new BoxSlider(
-    sliderEl,
+    getSliderElement(el),
     new FadeSlider({ timingFunction }),
+    boxSliderOptions,
+  )
+}
+
+export type CreateTileSliderOptions = Partial<BoxSliderOptions> &
+  TileSliderOptions
+
+export function createTileSlider(
+  el: string | HTMLElement,
+  options: CreateTileSliderOptions,
+) {
+  const { rowOffset, rows, tileEffect, ...boxSliderOptions } = options
+
+  return new BoxSlider(
+    getSliderElement(el),
+    new TileSlider({ rowOffset, rows, tileEffect }),
     boxSliderOptions,
   )
 }
